27ca19d794cd224b9690954e6f8c6e582ea014ba
Taras Gez <37795572+TarasGez@users.noreply.github.com>
2026-09-18 10:55:19 +0200

    refactor(ds): make label consume design system primitives

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,19 +2,8 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-function Label({
-  className,
-  ...props
-}: React.LabelHTMLAttributes<HTMLLabelElement>) {
-  return (
-    <label
-      className={cn(
-        "text-xs font-medium uppercase tracking-[0.18em] text-slate-400",
-        className,
-      )}
-      {...props}
-    />
-  );
+function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
+  return <label className={cn("ds-label-control", className)} {...props} />;
 }
 
 export { Label };
