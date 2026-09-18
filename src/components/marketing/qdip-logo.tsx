d7d002399583916a50167b313eb31c19d54e3800
Taras Gez <taraspixel@gmail.com>
2026-09-18 17:50:17 +0200

    fix: logo

--- src/components/marketing/qdip-logo.tsx
+++ src/components/marketing/qdip-logo.tsx
@@ -3,14 +3,19 @@ import styles from "./qdip-site.module.css";
 
 export function QdipLogo({ inverse = false }: { inverse?: boolean }) {
   return (
-    <span aria-label="QDIP" className={styles.logo} data-inverse={inverse || undefined} role="img">
+    <span
+      aria-label="QDIP"
+      className={styles.logo}
+      data-inverse={inverse || undefined}
+      role="img"
+    >
       <Image
         alt=""
         aria-hidden="true"
         className={styles.logoWordmark}
         height={157}
         priority
-        src="/qdip-logo.jpg"
+        src="/qdip-logo.png"
         width={300}
       />
     </span>
