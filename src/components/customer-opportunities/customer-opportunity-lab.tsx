6be44de6640f638569fcae2429d6df7e75ef4014
Taras Gez <37795572+TarasGez@users.noreply.github.com>
2026-09-19 19:49:21 +0200

    refactor: remove legacy customer opportunities wrapper

--- src/components/customer-opportunities/customer-opportunity-lab.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { CustomerOpportunitiesView } from "@/features/customer-opportunities/components/customer-opportunities-view";
-import { useCustomerOpportunitiesViewModel } from "@/features/customer-opportunities/hooks/use-customer-opportunities-view-model";
-
-export function CustomerOpportunityLab() {
-  const viewModel = useCustomerOpportunitiesViewModel();
-  return <CustomerOpportunitiesView vm={viewModel} />;
-}
